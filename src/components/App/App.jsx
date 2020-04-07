929e9ecf76e8b194be4a4eb437609ffb9c6c9465
SN <sacha.nocetto@hotmail.com>
2020-04-07 12:58:44 -0300

    added initial configuration * added webpack * added babel * added babel for arrow fuctions

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className="App-container">
+        <div className="App-content">
+          <h1>sesion del locro</h1>
+          <span>que buneo que esta el mate</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
