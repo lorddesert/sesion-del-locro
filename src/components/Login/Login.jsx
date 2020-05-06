5d76689bff7b4abb89b980c00afb013375fddbe6
Alan sacha <sacha.nocetto@hotmail.com>
2020-05-06 20:57:39 -0300

    login: *login function works. *detect when user is not registered. *detect password mistakes. *responsive added. *design finished.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,60 +1,81 @@
 import React, { Component } from 'react';
 
 import './Login.scss';
+import locro from './resources/locro.png';
 
 import PrimaryButton from '../PrimaryButton/PrimaryButton';
 import SecondaryButton from '../SecondaryButton/SecondaryButton';
 
 
 class Login extends Component {
+
+  state = {
+    showLogin: true
+  }
+
+  toggleShow = () => {
+    this.setState(state => ({
+      showLogin: !state.showLogin
+    }));
+  }
+
   render() {
-    if(this.props.showLoginOptions) {
       return (
         <div className='Login'>
-            <div className='Login-options'>
-              <div className='Login-option' >
-               <PrimaryButton className='Facebook' value='Facebook' action={this.props.authWithFB} />
+            <form className={this.props.showLogin ? 'Login-form' : 'Register-form'}>
+              <div className='topImg'>
+                <h2>S</h2>
               </div>
-                <SecondaryButton value='Volver' action={this.props.toggleShowLogin}/>
-            </div>
-        </div>
-      );
-    }
-    else if(this.props.showRegister) {
-      return (
-        <div className='Login'>
-            <div className='Login-options'>
-              <div className='Register-form'>
-                <div className='Form-label'>
-                  <span>Nombre de usuario</span>
-                  <input type='text'/>
-                </div>
-                <div className='Form-label'>
-                  <span>Contraseña</span>
-                  <input type='text'/>
-                </div>
+               <div className='top2Img'>
+                <img src={locro} alt='locro'></img>
+              </div>
+              <div className='title'>
+                {this.state.showLogin ?
+                <h2>Iniciar Sesión</h2>
+                :
+                <h2>Registrarse</h2>
+                }
+              </div>
+              <div className='errorMsg'>
+                <span id='errorMsg'></span>
+              </div>
+              <div className='userName-title'>
+                <span>Nombre de usuario</span>
+              </div>
+               <div className='userName-input'>
+                <input type='text' placeholder='Enter username' id='userName' required/>
+              </div>
+              <div className='passsword-title'>
+                <span>Contraseña</span>
+              </div>
+              <div className='password-input'>
+                <input type='password' placeholder='Enter password' id='password' required/>
+              </div>
+              <div className='submit-btn'>
+                {this.state.showLogin ?
+                  <PrimaryButton value='Iniciar Sesión' action={this.props.authUser} />
+                  :
+                  <PrimaryButton value='Registrate' action={this.props.authUser} />
+                }
+              </div>
+              <div className='register'>
                 <div>
-                  <PrimaryButton value='Registrate' action={() => alert('Hello World')} />
+                  {this.state.showLogin ?
+                    <p>¿No tienes una cuenta?, <span onClick={this.toggleShow}>¡Registrate!</span></p>
+                  :
+                    <p>¿Ya tienes una cuenta?, <span onClick={this.toggleShow}>¡Inicia sesión!</span></p>
+                  }
                 </div>
-                <SecondaryButton size='10px' value='Volver' action={this.props.toggleShowRegister} />
               </div>
-            </div>
+                <div className='bottomLetter'>
+                  <h2>S</h2>
+                </div>
+                <div className='bottom2Img'>
+                  <img src={locro} alt='locro'></img>
+                </div>
+            </form>
         </div>
       );
-    }
-    else {
-      return(
-        <div className='Login'>
-            <div className='Login-options'>
-              <div className='Login-option'>
-                <PrimaryButton value='Iniciar Sesión' action={this.props.toggleShowLogin}/>
-              <span style={{color: '#fff'}}>ó</span>
-                <SecondaryButton value='Registrate' action={this.props.toggleShowRegister}/>
-              </div>
-            </div>
-        </div>
-      );
-    }
   }
 }
 
