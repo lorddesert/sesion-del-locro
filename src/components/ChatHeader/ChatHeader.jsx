20df1a03f63bada00d10f06b3ecd760be39df5ca
Alan sacha <sacha.nocetto@hotmail.com>
2020-10-19 08:27:41 -0300

    design improved and organized

--- src/components/ChatHeader/ChatHeader.jsx
+++ src/components/ChatHeader/ChatHeader.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './ChatHeader.scss';
 import arrow from './resources/arrow.svg';
 import altImg from './resources/altuser.png';
+import moreOptions from './resources/more-options.svg';
+
 
 function toggleClass () {
   document.getElementById('main').classList.toggle('show-chat');
@@ -14,11 +16,17 @@ const ChatHeader = props => {
       <div className='return' id='return' onClick={toggleClass}>
         <img src={arrow} alt='Volver' />
       </div>
-      <div>
+      <div className='chatHeader-img'>
         <img className='receiver-img' src={props.receiverPhoto ? props.receiverPhoto : altImg} alt='foto del usuario'/>
       </div>
-      <div>
-        <h2>{props.stateMsg ? props.stateMsg : props.receiverName}</h2>
+      <div className='chatHeader-headings'>
+        <h1>{props.receiverName}</h1>
+        {props.stateMsg &&
+          <h2>{props.stateMsg}</h2>
+        }
+      </div>
+      <div className='more-options'>
+        <img src={moreOptions} alt="mas opciones" onClick={props.toggleModal}/>
       </div>
     </div>
   );
