5b8197832c8146627625ca21ec20aeac06b570e5
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-02-03 21:15:20 -0300

    initialized toastr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import 'toastr/build/toastr.min.js';
+import 'toastr/build/toastr.min.css';
 
 import App from './components/App/App';
 
