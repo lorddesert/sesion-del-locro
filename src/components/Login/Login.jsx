d4e16207fb84986479e2493f06191e6677093bb8
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-02-03 21:17:44 -0300

    login ready for firebase auth implementation
    login component is now ready for being used by auth for login, instead of the database.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,61 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Login.scss';
-import locro from './resources/locro.jpg';
-import logo from './resources/logo.svg';
+import "./Login.scss";
+import locro from "./resources/locro.jpg";
+import logo from "./resources/logo.svg";
 
-import PrimaryButton from '../PrimaryButton/PrimaryButton';
-import SecondaryButton from '../SecondaryButton/SecondaryButton';
-
+import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import SecondaryButton from "../SecondaryButton/SecondaryButton";
 
 class Login extends Component {
-
   state = {
-    showLogin: true
-  }
+    showLogin: true,
+  };
 
   toggleShow = () => {
-    this.setState(state => ({
-      showLogin: !state.showLogin
+    this.setState((state) => ({
+      showLogin: !state.showLogin,
     }));
-  }
+  };
 
-  handleEvent = e => {
+  handleEvent = (e) => {
     e.persist();
     e.preventDefault();
-    e.target.classList.toggle('focusedInput');
-  }
+    e.target.classList.toggle("focusedInput");
+  };
 
   render() {
-      return (
-        <div className='Login'>
-         <img  className='Login-img' src={locro} alt='locro background'/>
-          <div className='Login-form-container'>
-            <div className='Logo'>
-              <h1>Sesion del Locro</h1>
-              {/* <img src='https://frasesparami.com/wp-content/uploads/2019/09/imagenes-bonitas.jpg' alt='logo' width='50px' /> */}
-              <img src={logo} alt='logo' width='50px' />
-            </div>
-            <form className='Login-form'>
-              <span id='errorMsg'></span>
-              <label htmlFor='username'>
-                <h2>Nombre de usuario</h2>
-                <input type='text' id='username' onFocus={this.handleEvent} onBlur={this.handleEvent}/>
-              </label>
-              <label htmlFor='password'>
-                <h2>Contraseña</h2>
-                <input type='password' id='password' onFocus={this.handleEvent} onBlur={this.handleEvent} />
-              </label>
-              <label htmlFor='submit'>
-                <PrimaryButton id='submit' value='Iniciar Sesión' action={this.props.authUser}/>
-              </label>
-            <div className='links'>
-              <SecondaryButton id='link' value='¿No tienes una cuenta?' action={this.props.toggleShowRegister}/>
-            </div>
-            </form>
+    return (
+      <div className="Login">
+        <img className="Login-img" src={locro} alt="locro background" />
+        <div className="Login-form-container">
+          <div className="Logo">
+            <h1>Sesion del Locro</h1>
+            {/* <img src='https://frasesparami.com/wp-content/uploads/2019/09/imagenes-bonitas.jpg' alt='logo' width='50px' /> */}
+            <img src={logo} alt="logo" width="50px" />
           </div>
+          <form className="Login-form">
+            <span id="errorMsg"></span>
+            <label htmlFor="email">
+              <h2>Email</h2>
+              <input
+                type="text"
+                id="email"
+                onFocus={this.handleEvent}
+                onBlur={this.handleEvent}
+                placeholder="example@hotmail.com"
+              />
+            </label>
+            <label htmlFor="password">
+              <h2>Contraseña</h2>
+              <input
+                type="password"
+                id="password"
+                onFocus={this.handleEvent}
+                onBlur={this.handleEvent}
+              />
+            </label>
+            <label htmlFor="submit">
+              <PrimaryButton
+                id="submit"
+                value="Iniciar Sesión"
+                action={(e) => {
+                  e.preventDefault();
+                  this.props.login();
+                }}
+              />
+            </label>
+            <div className="links">
+              <SecondaryButton
+                id="link"
+                value="¿No tienes una cuenta?"
+                action={this.props.toggleShowRegister}
+              />
+            </div>
+          </form>
         </div>
-      );
+      </div>
+    );
   }
 }
 
