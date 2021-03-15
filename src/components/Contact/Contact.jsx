f3802799bd9380f7bcea670581050da33b492665
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-03-15 13:26:39 -0300

    auth user info implemented

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Contact.scss';
-import altUserImg from './resources/altuser.png';
+import "./Contact.scss";
+import altUserImg from "./resources/altuser.png";
 
 class Contact extends Component {
-
   render() {
     return (
-      <div className='Contact' onClick={this.props.setChat && (() => this.props.setChat(this.props.contact.userName, this.props.contact.ref))}>
-        <div className={this.props.contact.online ? 'Contact-avatar online' : 'Contact-avatar'}>
-          {this.props.contact.photo ?
-            <img id={`contact-${this.props.number}`} src={this.props.contact.photo} alt='contact image'></img>
-            :
-            <div className='alternative-img'>
-              <img id={`contact-${this.props.number}`} src={altUserImg} alt={this.props.contact.userName} style={{background: '#e3e3e3'}} />
-            </div>
+      <div
+        className="Contact"
+        onClick={
+          this.props.setChat &&
+          (() =>
+            this.props.setChat(
+              this.props.contact.nickname,
+              this.props.contact.ref
+            ))
+        }
+      >
+        <div
+          className={
+            this.props.contact.online
+              ? "Contact-avatar online"
+              : "Contact-avatar"
           }
+        >
+          {this.props.contact.photo ? (
+            <img
+              id={`contact-${this.props.number}`}
+              src={this.props.contact.photo}
+              alt="contact image"
+            ></img>
+          ) : (
+            <div className="alternative-img">
+              <img
+                id={`contact-${this.props.number}`}
+                src={altUserImg}
+                alt={this.props.contact.nickname}
+                style={{ background: "#e3e3e3" }}
+              />
+            </div>
+          )}
         </div>
-        <div className='Contact-name'>
-          <span>{this.props.contact.userName}</span>
+        <div className="Contact-name">
+          <span>{this.props.contact.nickname}</span>
         </div>
       </div>
     );
