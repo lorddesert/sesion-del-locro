dd8dd8fe3495b786be61669457fa0c7105f2c48b
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-09-29 02:29:32 -0300

    updated createCRModal

--- src/components/CreateCRModal/CreateCRModal.jsx
+++ src/components/CreateCRModal/CreateCRModal.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
+import GlobalContext from '../../context/GlobalContext'
+import handleInputFocus from '../../scripts/handleInputFocus'
 
 import Modal from "../Modal/Modal";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
@@ -6,10 +8,88 @@ import SecondaryButton from "../SecondaryButton/SecondaryButton";
 
 import "./CreateCRModal.scss";
 
+
+
+const { globalContext } = useContext(GlobalContext)
+
+const { app } = globalContext
+
+const toggleModal = useCallback((createChatRoom = false) => {
+  if (createChatRoom) {
+    this.setState(
+      () => ({
+        showCRModal: true,
+        inChatRoom: false,
+        receiver: false,
+      }),
+      () => {
+        document.getElementById("modal").classList.toggle("show-modal");
+        setTimeout(
+          () =>
+            document
+              .getElementById("modalForm")
+              .classList.toggle("modalTransition"),
+          100
+        );
+      }
+    );
+  } else {
+    this.setState(
+      () => ({
+        showCRModal: true,
+      }),
+      () => {
+        document.getElementById("modal").classList.toggle("show-modal");
+        setTimeout(
+          () =>
+            document
+              .getElementById("modalForm")
+              .classList.toggle("modalTransition"),
+          10
+        );
+      }
+    );
+  }
+}, [])
+
+const createNewChatRoom = useCallback((e) => {
+  e.persist();
+  e.preventDefault();
+
+  const ref = app.database().ref("chatRooms");
+  const minDiceValue = document.getElementById("minValue").value;
+  const maxDiceValue = document.getElementById("maxValue").value;
+  const name = document.getElementById("chatRoomName").value;
+  const newChatRoom = {
+    // photo,
+    chat: [],
+    name,
+    minDiceValue,
+    maxDiceValue,
+    stateMsg: "",
+  };
+
+  ref
+    .once("value")
+    .then((snapshot) => {
+      if (snapshot.child(`${name}`).exists()) {
+        alert("El nombre de la sala no esta disponible.");
+        return false;
+      } else
+        ref
+          .child(`${name}`)
+          .set(newChatRoom)
+          .then(() => {
+            toggleModal();
+          });
+    })
+    .catch((err) => console.log(err));
+}, []);
+
 const CreateCRModal = (props) => {
   if (props.inChatRoom) {
     return (
-      <Modal closeModal={props.toggleModal}>
+      <Modal closeModal={toggleModal}>
         <h2>Nombre de la sala</h2>
         <label htmlFor="chatRoom"></label>
         <input
@@ -17,8 +97,8 @@ const CreateCRModal = (props) => {
           id="chatRoomName"
           name="chatRoom"
           required
-          onFocus={props.handleInputFocus}
-          onBlur={props.handleInputFocus}
+          onFocus={handleInputFocus}
+          onBlur={handleInputFocus}
         />
         <h2>Mensaje de estado</h2>
         <label htmlFor="chatRoom"></label>
@@ -27,8 +107,8 @@ const CreateCRModal = (props) => {
           id="chatRoomState"
           name="chatRoom"
           required
-          onFocus={props.handleInputFocus}
-          onBlur={props.handleInputFocus}
+          onFocus={handleInputFocus}
+          onBlur={handleInputFocus}
         />
         <label htmlFor="diceFaces"></label>
         <h2>Caras del dado</h2>
@@ -41,14 +121,14 @@ const CreateCRModal = (props) => {
           <input type="number" id="maxValue" />
         </div>
         <div style={{ display: "flex", placeItems: "center" }}>
-          <SecondaryButton value="Cancelar" action={props.toggleModal} />
-          <PrimaryButton value="Guardar" action={props.modifyChatRoom} />
+          <SecondaryButton value="Cancelar" action={toggleModal} />
+          <PrimaryButton value="Guardar" action={modifyChatRoom} />
         </div>
       </Modal>
     );
   } else {
     return (
-      <Modal closeModal={props.toggleModal}>
+      <Modal closeModal={toggleModal}>
         <h2>Nombre de la sala</h2>
         <label htmlFor="chatRoom"></label>
         <input
@@ -56,8 +136,8 @@ const CreateCRModal = (props) => {
           id="chatRoomName"
           name="chatRoom"
           required
-          onFocus={props.handleInputFocus}
-          onBlur={props.handleInputFocus}
+          onFocus={handleInputFocus}
+          onBlur={handleInputFocus}
         />
         <label htmlFor="diceFaces"></label>
         <h2>Caras del dado</h2>
@@ -70,8 +150,8 @@ const CreateCRModal = (props) => {
           <input type="number" id="maxValue" />
         </div>
         <div style={{ display: "flex", placeItems: "center" }}>
-          <SecondaryButton value="Cancelar" action={props.toggleModal} />
-          <PrimaryButton value="Crear" action={props.createNewChatRoom} />
+          <SecondaryButton value="Cancelar" action={toggleModal} />
+          <PrimaryButton value="Crear" action={createNewChatRoom} />
         </div>
       </Modal>
     );
