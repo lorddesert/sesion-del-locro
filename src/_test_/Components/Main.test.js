6807e2c12628df3cfe6b046b080d2279ddb45d4e
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-09-30 07:37:06 -0300

    Main component test added

---
+++ src/_test_/Components/Main.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import '@testing-library/jest-dom'
+
+ import * as React from 'react'
+ import { render } from '@testing-library/react'
+ import Main from'../../components/Main/Main'
+ 
+   test('Rendering component Main: ', () => {
+     render(<Main />)
+   })
