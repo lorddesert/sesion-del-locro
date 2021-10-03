6937d3ec40433b5e328abe965538301eca8b0406
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-10-03 13:13:01 -0300

    formated

--- src/components/ChatRoom/ChatRoom.jsx
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -9,73 +9,59 @@ import Context from '../../context/GlobalContext'
 const ChatRoom = props => {
 
   console.log(props)
-  
-const { globalContext, setGlobalContext } = useContext(Context)
-const { setChat, inChatRoom } = globalContext
 
-  const setChatRoom = () => {
-    // console.log(props.chatRoom.name)
-    console.log('ininChatRoom', inChatRoom)
+  const { globalContext, setGlobalContext } = useContext(Context)
+  const { setChat, inChatRoom } = globalContext
+
+  const setChatRoom = async () => {
+    try {
+      console.log(props.chatRoom)
+      console.log('ininChatRoom', inChatRoom)
 
 
-    if (window.innerWidth < 768)
-      document.getElementById("main").classList.toggle("show-chat");
+      if (window.innerWidth < 768)
+        document.getElementById("main").classList.toggle("show-chat");
 
-    // for (i = 0; i < this.state.chatRooms.length; i++)
-    //   if (this.state.chatRooms[i].name === receiver)
-        setGlobalContext({
-          ...globalContext,
-          inChatRoom: true,
-          chat: [...props.chatRoom.chat],
-          receiver: {
-            ...props.chatRoom
-          }
-        })
-        setChat([...props.chatRoom.chat])
+      setGlobalContext({
+        ...globalContext,
+        inChatRoom: true,
+        chat: [ ...props.chatRoom.chat ],
+        receiver: {
+          ...props.chatRoom
+        }
+      })
+      await setChat([ ...props.chatRoom.chat ])
+      scrollBottom()
 
-        setTimeout(() => console.log(globalContext), 1000)
-
-    //     this.setState(
-    //       {
-    //         receiver,
-    //         receiverName: chatRoom.name,
-    //         chat: this.state.chatRooms[i].chat,
-    //         inChatRoom: true,
-    //         stateMsg: this.state.chatRooms[i].stateMsg,
-    //         diceValues: {
-    //           min: this.state.chatRooms[i].minDiceValue,
-    //           max: this.state.chatRooms[i].maxDiceValue,
-    //         },
-    //       },
-    //       scrollBottom()
-    //     );
-    
+    } catch (error) {
+      console.log(error)
+    }
   };
 
-    return (
-      <div className="ChatRoom" onClick={setChatRoom}>
-        <div className="ChatRoom-image">
-          {props.chatRoom.photo ?
+  return (
+    <div className="ChatRoom" onClick={setChatRoom}>
+      <div className="ChatRoom-image">
+        {props.chatRoom.photo ?
+          <img
+            id={`ChatRoom-${props.number}`}
+            src={props.chatRoom.photo}
+            alt='ChatRoom image'
+          />
+          :
+          <div className='alternative-img'>
             <img
               id={`ChatRoom-${props.number}`}
-              src={props.chatRoom.photo}
-              alt='ChatRoom image'
+              src={chatRoomImg} alt={props.chatRoom.name}
+              style={{ background: '#e3e3e3' }}
             />
-            :
-            <div className='alternative-img'>
-              <img
-                id={`ChatRoom-${props.number}`}
-                src={chatRoomImg} alt={props.chatRoom.name}
-                style={{background: '#e3e3e3'}}
-              />
-            </div>
-          }
-        </div>
-        <div className="ChatRoom-name">
-          <span>{props.chatRoom.name}</span>
-        </div>
+          </div>
+        }
       </div>
-    );
+      <div className="ChatRoom-name">
+        <span>{props.chatRoom.name}</span>
+      </div>
+    </div>
+  );
 
 }
 
