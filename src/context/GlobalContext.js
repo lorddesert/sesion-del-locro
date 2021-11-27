0ffe87783e04f81e45154ca5b57fb77a6b331369
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-11-27 18:39:13 -0300

    changed firebase imports

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 import { initializeApp } from "firebase/app"
+import { 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword,
+    currentUser,
+    signOut,
+} from "firebase/auth"
 import { DB_CONFIG } from '../config/config'
 
 
 const app = initializeApp(DB_CONFIG)
 const storage = app.storage
-const auth = app.auth() // I need the reference here?
+const auth = {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    currentUser,
+} // I need the reference here?
 
 const initialContext = {
     app,
