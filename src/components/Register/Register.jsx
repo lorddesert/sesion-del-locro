3063eff2eb26d6a94bef7d9bcb5674232e3f3381
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-01 22:56:23 -0300

    formatted

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,79 +1,86 @@
-import React, { useContext, useState } from "react"
-import { getAuth, updateCurrentUser, updateProfile } from 'firebase/auth'
-import { getDatabase, ref, child, set } from 'firebase/database'
+import React, { useContext, useState } from "react";
+import { getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
+import { getDatabase, ref, child, set } from "firebase/database";
 
 // Components
-import Context from '../../context/GlobalContext'
-import "./Register.scss"
-import PrimaryButton from "../PrimaryButton/PrimaryButton"
-import SecondaryButton from "../SecondaryButton/SecondaryButton"
+import Context from "../../context/GlobalContext";
+import "./Register.scss";
+import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import SecondaryButton from "../SecondaryButton/SecondaryButton";
 
-const Register = props => {
-  const { globalContext } = useContext(Context)
-  const { app, auth, logIn, user } = globalContext
-  const [ img, setImg ] = useState("")
+const Register = (props) => {
+  const { globalContext } = useContext(Context);
+  const { app, auth, logIn, user } = globalContext;
+  const [img, setImg] = useState("");
 
   const getRegisterImg = (e) => {
-    e.persist()
-    e.preventDefault()
+    e.persist();
+    e.preventDefault();
 
-    const img = document.getElementById("fileInput").files[ 0 ]
-    const imgURL = URL.createObjectURL(img)
-    setImg(imgURL)
-  }
+    const img = document.getElementById("fileInput").files[0];
+    const imgURL = URL.createObjectURL(img);
+    setImg(imgURL);
+  };
 
   const register = async (e) => {
     try {
-      e.preventDefault()
-      const email = document.querySelector("#email").value
-      const nickname = document.querySelector("#nickname").value
-      const password = document.querySelector("#password").value
+      e.preventDefault();
+      const email = document.querySelector("#email").value;
+      const nickname = document.querySelector("#nickname").value;
+      const password = document.querySelector("#password").value;
 
       // !LOADING
-      await auth.createUserWithEmailAndPassword(getAuth(), email, password)
+      await auth.createUserWithEmailAndPassword(getAuth(), email, password);
 
-      
       await updateProfile(getAuth().currentUser, {
-        displayName: nickname
-      })
+        displayName: nickname,
+      });
 
-      console.log('%c Update displayName', 'color: aquagreen; background: black; border-radius: 7px;');
-      
-      const currentUserRef = child(ref(getDatabase(), "users"), `${getAuth().currentUser.uid}`)
-      console.log('%c Current user ref', 'color: aquagreen; background: black; border-radius: 7px;');
+      console.log(
+        "%c Update displayName",
+        "color: aquagreen; background: black; border-radius: 7px;"
+      );
+
+      const currentUserRef = child(
+        ref(getDatabase(), "users"),
+        `${getAuth().currentUser.uid}`
+      );
+      console.log(
+        "%c Current user ref",
+        "color: aquagreen; background: black; border-radius: 7px;"
+      );
 
       // return
       await set(currentUserRef, {
         nickname: nickname,
         online: true,
         email,
-      })
-      console.log('FLAG 2 Set new user info');
+      });
+      console.log("FLAG 2 Set new user info");
       console.log(getAuth().currentUser);
-      
 
-      alert('SU! ccesfully registered. Welcome!')
+      alert("SU! ccesfully registered. Welcome!");
 
-      logIn(true)
+      logIn(true);
     } catch (error) {
       switch (error.code) {
         case "auth/weak-password":
-          alert("La contraeña es muy debil, intente usando otra.")
-          break
+          alert("La contraeña es muy debil, intente usando otra.");
+          break;
         case "auth/email-already-in-use":
-          alert("El email ya esta en uso, pruebe con otro.")
-          break
+          alert("El email ya esta en uso, pruebe con otro.");
+          break;
         case "auth/operation-not-allowed":
-          alert("Email o contraseña no validos.")
-          break
+          alert("Email o contraseña no validos.");
+          break;
 
         default:
-          alert("Un error ha ocurrido, intente en unos minutos.")
-          console.log(error)
-          break
+          alert("Un error ha ocurrido, intente en unos minutos.");
+          console.log(error);
+          break;
       }
     }
-  }
+  };
 
   const handleInputFocus = (e) => {
     e.persist();
@@ -82,15 +89,14 @@ const Register = props => {
   };
 
   const returnToLogin = (e) => {
-    e.preventDefault()
-    props.setShowRegister(false)
-    props.setShowLogin(true)
-  }
+    e.preventDefault();
+    props.setShowRegister(false);
+    props.setShowLogin(true);
+  };
 
   return (
     <>
       <main className="Register">
-
         <form className="RegisterForm">
           <section>
             <label htmlFor="email">
@@ -119,7 +125,7 @@ const Register = props => {
                 onFocus={handleInputFocus}
                 onBlur={handleInputFocus}
                 placeholder="Imma'getchu"
-                />
+              />
             </label>
           </section>
 
@@ -138,13 +144,14 @@ const Register = props => {
             </label>
           </section>
 
-          <div style={{ display: "flex", placeItems: "center", margin: "0 auto" }}>
+          <div
+          >
             {img && <img src={img} alt="imagen del usuario" />}
             <PrimaryButton
               action={(e) => {
-                e.persist()
-                e.preventDefault()
-                document.querySelector("#fileInput").click()
+                e.persist();
+                e.preventDefault();
+                document.querySelector("#fileInput").click();
               }}
               value="Cambiar imagen"
               className="changeImgBtn"
@@ -156,20 +163,22 @@ const Register = props => {
               onChange={getRegisterImg}
             />
           </div>
-          <PrimaryButton value="Listo" action={register} /> {/*//! End register */}
-
-          <p>
-          <strong style={{fontSize: "20px", fontWeight: "bold", color: 'white'}}>ó</strong>
-        </p>
-        <SecondaryButton
-          id="logIn"
-          value="Inicia sesión"
-          action={returnToLogin}
-        />
+          <p style={{margin: "1em 0"}}>
+            <strong
+              style={{fontSize: "16px", fontWeight: "bold", color: "white" }}
+            >
+              ó
+            </strong>
+          </p>
+          <PrimaryButton
+            id="logIn"
+            value="Inicia sesión"
+            action={returnToLogin}
+          />
         </form>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
