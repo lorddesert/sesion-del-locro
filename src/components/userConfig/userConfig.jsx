ea211029d80ed0a77ab3b91e17972f32f13b1b7d
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-03 15:05:29 -0300

    update component

--- src/components/userConfig/userConfig.jsx
+++ src/components/userConfig/userConfig.jsx
@@ -1,173 +1,141 @@
-import React, { useState, useCallback, useContext } from "react";
-import Context from '../../context/GlobalContext'
+import React, { useState} from "react"
+import { getAuth, updateProfile, updatePassword } from "firebase/auth"
+import { ref, getDatabase, update } from "firebase/database"
+import { uploadBytes, getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage'
 
-import "./userConfig.scss";
-import closeImg from "./resources/close.svg";
-import PrimaryButton from "../PrimaryButton/PrimaryButton";
-import SecondaryButton from "../SecondaryButton/SecondaryButton";
+import "./userConfig.scss"
+import PrimaryButton from "../PrimaryButton/PrimaryButton"
 
-const userConfig = props => {
-
- const { globalContext } = useContext(Context);
-
-  const { app, auth, user } = globalContext
+const userConfig = (props) => {
   
-  const { photoURL, displayName, email } = user
+  const { displayName, uid, photoURL } = getAuth().currentUser
+  const [ userImage, setUserImage ] = useState(photoURL)
 
-  // if('globalContext' in context) context = {...context.globalContext}
+  const saveNewPassword = async (e) => {
 
-  const [edit, setEdit] = useState(false)
-
-  const toggleShowUserCfg = useCallback((e) => {
-    if(e)
-    e.preventDefault()
-    setEdit(!!edit)
-
-    const userCfgRef = document.querySelector("#userConfig");
-    let userInfoContainer = document.querySelector("#userInfoContainer");
-
-    if (!userInfoContainer.classList.contains("closed")) 
-      userInfoContainer.classList.toggle("closed");
-
-    userCfgRef.classList.toggle("toggleUserConfig");
-  }, []);
-
-  const toggleEditForm = useCallback((e) => {
-    e.preventDefault()
-
-    setEdit(!edit)
-  }, []);
-
-  const saveNewUserInfo = async (e) => {
-    e.preventDefault()
+    try {
+      e.preventDefault()
   
-    const newNickname = document.querySelector("#newdisplayName").value
-    const newPassword = document.querySelector("#newPassword").value
-    const confirmPassword = document.querySelector("#confirmPassword").value
-    const user = auth.currentUser
-    
-    const userRef = app
-      .database()
-      .ref(`users/${user.uid}`)
-    const res = await userRef.child("nickname").once("value")
-    const oldNickname = res.val()
-
+      const newPassword = document.querySelector("#newPassword").value
+      const confirmPassword = document.querySelector("#confirmPassword").value
+  
       if (newPassword !== "")
-        if (newPassword === confirmPassword)
-          user.updatePassword(newPassword)
+        if (newPassword === confirmPassword) await updatePassword(getAuth().currentUser, newPassword)
 
         else {
           alert("¡Las contraseñas deben ser identicas!")
           return false
         }
+  
+      alert("Contraseña actualizada!")
       
-      if (oldNickname !== newNickname) {
-        await user.updateProfile({displayName: newNickname})
-        userRef.child("nickname").set(newNickname)
-      }
-
-      alert("Usuario actualizado!")
-      toggleShowUserCfg()
+    } catch (error) {
+      
+      alert('BAD!')
+      console.log(error)
+    }
+    toggleShowUserCfg()
   }
 
-  //* Folded
-    if (!edit)
-      return (
-        <div className="userConfig" id="userConfig">
-          <div className="exitImg">
-            <img src={closeImg} alt="exit" onClick={toggleShowUserCfg} />
-          </div>
-          <div className="userInfoContainer closed" id="userInfoContainer">
-            <div className="userInfo">
-              <div className="userProfile">
-                <img src={photoURL} alt={displayName} />
-                <div className="displayInfo">
-                  <hgroup>
-                    <h2>Email</h2>
-                    <h3>{email}</h3>
-                  </hgroup>
-                  <hgroup>
-                    <h2>Nickname</h2>
-                    <h3>{displayName}</h3>
-                  </hgroup>
-                </div>
-              </div>
-              <PrimaryButton
-                action={toggleEditForm}
-                value="Editar"
-                id="editBtn"
-                className="primaryConfigBtn"
-              />
-            </div>
-          </div>
+  const saveProfileInfo = async (e) => {
+    try {
+      e.preventDefault()
+
+      const newNickname = document.querySelector("#newdisplayName").value
+      const userRef = ref(getDatabase(), `users/${uid}`)
+      const userStorageRef = storageRef(getStorage(), `users/${uid}`)
+      const response = await uploadBytes(userStorageRef, userImage)
+      const imageURL = await getDownloadURL(response.ref)
+      // Update the Database User
+
+      console.log(imageURL)
+
+      await update(userRef, { nickname: newNickname, photo: imageURL })
+
+      // Update the Auth user 
+      console.log('FLAG 2')
+      await updateProfile(getAuth().currentUser, { displayName: newNickname, photoURL: imageURL  })
+
+      alert("Usuario actualizado!")
+    } catch (error) {
+      alert("BAD!")
+      console.log(error)
+    }
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    document.getElementById("fileInput").click()
+  }
+
+  const changePreviewImage = () => {
+    const file = document.querySelector('#fileInput').files[0]
+
+    let fileURL = URL.createObjectURL(file)
+
+    setUserImage(fileURL)
+  }
+
+  return (
+    <div className="userConfig" id="userConfig">
+      <form id="editForm">
+        <h2 style={{marginBottom: '1.5em'}}>Profile</h2>
+        <div className="profileImage">
+          <img src={userImage} alt='imagen de perfil'/>
+          <PrimaryButton
+            action={handleClick}
+            value="Cambiar imagen"
+            className="changeImgBtn"
+          />
         </div>
-      );
-      
-    //* Unfolded
-    else
-      return (
-        <div className="userConfig" id="userConfig">
-          <div className="exitImg">
-            <img src={closeImg} alt="exit" onClick={toggleShowUserCfg} />
-          </div>
-          <div className="userInfoContainer" id="userInfoContainer">
-            <div className="userInfo">
-              <div className="userProfile">
-                <img src={photoURL} alt={displayName} />
-                <PrimaryButton
-                  action={() => document.getElementById("fileInput").click()}
-                  value="Cambiar imagen"
-                  className="changeImgBtn"
-                />
-                <input
-                  type="file"
-                  style={{ display: "none" }}
-                  id="fileInput"
-                  onChange={props.storageImg} //! NEED SUPPORT
-                />
-              </div>
-              <form id="editForm">
-                <label htmlFor="newdisplayName">
-                  <h2>Nickname</h2>
-                  <input
-                    type="text"
-                    id="newdisplayName"
-                    defaultValue={displayName}
-                  />
-                </label>
-                <label htmlFor="newPassword">
-                  <h2>Nueva contraseña</h2>
-                  <input
-                    type="text"
-                    id="newPassword"
-                    pattern="{6,}"
-                    placeholder="6 caracteres minimo."
-                  />
-                </label>
-                <label htmlFor="confirmPassword">
-                  <h2>Confirmar contraseña</h2>
-                  <input
-                    type="text"
-                    id="confirmPassword"
-                    pattern="{6,}"
-                    placeholder="6 caracteres minimo."
-                  />
-                </label>
-                <div className="formButtons">
-                  <SecondaryButton
-                    action={toggleShowUserCfg}
-                    value="Cancelar"
-                  />
-                  <PrimaryButton
-                    action={saveNewUserInfo} //! NEED SUPPORT
-                    value="Guardar"
-                  />
-                </div>
-              </form>
-            </div>
-          </div>
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="fileInput"
+          onChange={changePreviewImage}
+        />
+
+        <p>Nickname</p>
+        <label htmlFor="newdisplayName">
+          <input type="text" id="newdisplayName" defaultValue={displayName} />
+        </label>
+        <div className="formButtons">
+          <PrimaryButton
+            action={saveProfileInfo}
+            value="Guardar"
+          />
         </div>
-      );
+      </form>
+
+      <form id="editForm">
+        <h2>Security</h2>
+        <p>Nueva contraseña</p>
+        <label htmlFor="newPassword">
+          <input
+            type="password"
+            id="newPassword"
+            pattern="{6,}"
+            placeholder="6 caracteres minimo."
+          />
+        </label>
+        <p>Confirmar contraseña</p>
+        <label htmlFor="confirmPassword">
+          <input
+            type="password"
+            id="confirmPassword"
+            pattern="{6,}"
+            placeholder="6 caracteres minimo."
+          />
+        </label>
+        <div className="formButtons">
+          <PrimaryButton
+            action={saveNewPassword} //! NEED SUPPORT
+            value="Guardar"
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
 
-export default userConfig;
+export default userConfig
